ee02cb1ed24520317bd509c250da2a68d5f286ee
fadilahistq <114170254+fadilahistq@users.noreply.github.com>
2022-10-09 22:48:00 +0700

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -4,28 +4,29 @@ USE praktisi;
 
 #Buat Tabel Mahasiswa
 
-CREATE TABLE mahasiswa (
-	id_mahasiswa INT(10) AUTO_INCREMENT PRIMARY KEY,
-	full_name VARCHAR(100),
-	address VARCHAR(100) NOT NULL,
-	major VARCHAR(100) NOT NULL
+CREATE TABLE mahasiswa ( 
+	id_mahasiswa INT (10) AUTO_INCREMENT PRIMARY KEY,
+	full_name VARCHAR (100),
+	address VARCHAR (100) NOT NULL,
+	major VARCHAR (100) NOT NULL
 );
 
 #Buat Tabel Matkul
 
 CREATE TABLE matkul (
-	id_matkul INT(10) AUTO_INCREMENT PRIMARY KEY,
-	nama VARCHAR(100),
-	durasi INT(10) DEFAULT 0
+	id_matkul INT (10) AUTO_INCREMENT PRIMARY KEY,
+	nama_matkul VARCHAR (100),
+	durasi INT (10) DEFAULT 0
 );  
 
 #buat tabel jadwal
 
-# ntah kenapa ga jalan
--- CREATE TABLE jadwal (
--- 	id_jadwal INT(10) AUTO_INCREMENT PRIMARY KEY,
--- 	tanggal DATE NOT NULL,
--- 	jam TIME NOT NULL,
--- 	FOREIGN KEY (id_matkul) REFERENCES matkul(id_matkul),
--- 	FOREIGN KEY (id_mahasiswa) REFERENCES mahasiswa(id_mahasiswa)
--- );
+CREATE TABLE jadwal (
+	id_jadwal INT (10) AUTO_INCREMENT PRIMARY KEY,
+	id_matkul INT (10),
+	id_mahasiswa INT (10),
+	tanggal DATE NOT NULL,
+	jam TIME NOT NULL,
+	FOREIGN KEY (id_matkul) REFERENCES matkul (id_matkul),
+	FOREIGN KEY (id_mahasiswa) REFERENCES mahasiswa (id_mahasiswa)
+);
